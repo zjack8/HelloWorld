989d4cfce819860f9886b41b52a7427f5a3c5b12
zjack8 <61671353+zjack8@users.noreply.github.com>
2023-11-22 15:07:38 +1300

    supressed warning

--- Java/TicTacToe.java
+++ Java/TicTacToe.java
@@ -89,10 +89,11 @@ public class TicTacToe {
         // TODO
         for (int row = 0; row < 3; row++) {
             for (int column = 0; column < 3; column++) {
-                
 
             }
         }
+        return false;
+
     }
 
     private static void printBoard() {
