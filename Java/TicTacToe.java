529e65f9e4428ba5c6cb4f428477eb563eb17815
zjack8 <61671353+zjack8@users.noreply.github.com>
2023-11-23 07:50:13 +1300

    nearly

--- Java/TicTacToe.java
+++ Java/TicTacToe.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class TicTacToe {
@@ -7,7 +8,6 @@ public class TicTacToe {
                                        {" ", " ", " "}};
     private static String userTeam = " ";
     private static String cpuTeam = " ";
-    private static boolean gameOn = true;
     private static String winner = " ";
 
     private static final Scanner SC = new Scanner(System.in);  
@@ -16,84 +16,122 @@ public class TicTacToe {
         // Initialise Game        
         int turn = chooseTeam();
 
-        while (gameOn) {
+        System.out.println("===============");
+        while (isGameOver()) {
 
             nextTurn(turn++);
             printBoard();
-            
-            gameOn = isGameOver();
         }
 
     }
 
     private static int chooseTeam() {
-        System.out.print("Enter Team (X or O): ");
-        String input = SC.nextLine();
+        while (true) {
+            System.out.print("Enter Team (X or O): ");
+            String input = SC.nextLine();
+    
+            if ("X".equals(input)) {
+                userTeam = "X";
+                cpuTeam = "O";
+                return -1;
+            } else if ("O".equals(input)) {
+                userTeam = "O";
+                cpuTeam = "X";
+                return 0;
+            } else {
+                System.out.println("Wrong Input! Try Again");
+            }
+        }
+    }
+    
 
-        if (input == "X") {
-            userTeam = "X";
-            cpuTeam = "O";
-            return -1;
-        } else if (input == "O") {
-            userTeam = "O";
-            cpuTeam = "X";
-            return 0;
+    private static void nextTurn(int turn) {
+        if (turn % 2 == 0) {
+            userTurn();
         } else {
-            System.out.println("Wrong Input! Try Again");
-            return chooseTeam();
+            cpuTurn();
+        }
+
+    }
+
+    private static void userTurn() {
+        while (true) {
+            try {
+                System.out.print("Enter Position: ");
+                String stringPos = SC.nextLine();
+                int result = Integer.parseInt(stringPos);
+                if (applyIfValid(result)) {
+                    return;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Wrong Input! Try Again");
+            }
         }
     }
 
-    private static void nextTurn(int turn) {
-        int pos = 0;
-        if (turn % 2 == 0) {
-            pos = userTurn();
-        } else {
-            pos = cpuTurn();
+    private static void cpuTurn() {
+        System.out.println("CPU's Response:");
+        while (true) {
+            int result = getRandomNumberInRange(1, 9);
+            if (applyIfValid(result)) {
+                return;
+            }
         }
+    }
 
+    private static boolean applyIfValid(int pos) {
         int count = 0;
         for (int row = 1; row <= 3; row++) {
             for (int column = 1; column <= 3; column++) {
                 count++;
-                if (count == pos) {
+                if (count == pos && " ".equals(board[row][column])) {
                     board[row][column] = userTeam;
+                    return true;
                 }
             }
         }
-    }
-
-    private static int userTurn() {
-
-        System.out.print("Enter Position: ");
-        String stringPos = SC.nextLine();
-
-        try {
-            int result = Integer.parseInt(stringPos);
-            return result;
-        } catch (NumberFormatException e) {
-            System.out.println("Wrong Input! Try Again");
-            return userTurn();
-        }
-        
-    }
-
-    private static int cpuTurn() {
-        // TODO
-        System.out.println("CPU's Response:");
-        return 1;
-
+        return false;
     }
 
     private static boolean isGameOver() {
-        // TODO
-        for (int row = 0; row < 3; row++) {
-            for (int column = 0; column < 3; column++) {
+        return checkRows(board) || checkColumns(board) || checkDiagonals(board) || isBoardFull(board);
+    }
 
+    private static boolean checkRows(String[][] board) {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0].equals(board[i][1]) && board[i][1].equals(board[i][2]) && !board[i][0].equals(" ")) {
+                return true;
             }
         }
         return false;
+    }
 
+    private static boolean checkColumns(String[][] board) {
+        for (int i = 0; i < 3; i++) {
+            if (board[0][i].equals(board[1][i]) && board[1][i].equals(board[2][i]) && !board[0][i].equals(" ")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean checkDiagonals(String[][] board) {
+        if ((board[0][0].equals(board[1][1]) && board[1][1].equals(board[2][2]) && !board[0][0].equals(" ")) ||
+            (board[0][2].equals(board[1][1]) && board[1][1].equals(board[2][0]) && !board[0][2].equals(" "))) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isBoardFull(String[][] board) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j].equals(" ")) {
+                    return false; // If there is an empty space, the board is not full
+                }
+            }
+        }
+        return true; // All spaces are filled, and no winner found
     }
 
     private static void printBoard() {
@@ -108,5 +146,12 @@ public class TicTacToe {
         System.out.println("===============");
     }
 
+    private static int getRandomNumberInRange(int min, int max) {
+        if (min >= max) {
+            throw new IllegalArgumentException("Max must be greater than min");
+        }
 
+        Random random = new Random();
+        return random.nextInt((max - min) + 1) + min;
+    }
 }
