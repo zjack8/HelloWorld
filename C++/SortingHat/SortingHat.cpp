9a3e7f6dae3c2e2cdd1a05c7ec36efa9237a6c42
zjack8 <61671353+zjack8@users.noreply.github.com>
2023-11-27 11:28:08 +1300

    main method complete

--- C++/SortingHat/SortingHat.cpp
+++ C++/SortingHat/SortingHat.cpp
@@ -18,15 +18,35 @@ void quickSort(std::vector<int>& arr, int left, int right) {
     // TODO
 }
 
+void bubbleSort(std::vector<int>& arr, int left, int right) {
+    // TODO
+}
+
+void selectionSort(std::vector<int>& arr, int left, int right) {
+    // TODO
+}
+
+void insertionSort(std::vector<int>& arr, int left, int right) {
+    // TODO
+}
+
+void heapSort(std::vector<int>& arr, int left, int right) {
+    // TODO
+}
+
+void radixSort(std::vector<int>& arr, int left, int right) {
+    // TODO
+}
+
 int main(int argc, char* argv[]) {
-    std::vector<int> defaultArray = {12, 11, 13, 5, 6, 7};
+    std::vector<int> defaultArray = {3, 8, 1, 6, 4, 9, 2, 7, 10, 5};
 
     // Determine which sorting algorithm to use based on the first argument
     std::string algorithm;
     if (argc > 1) {
         algorithm = argv[1];
     } else {
-        std::cerr << "Usage: " << argv[0] << " algorithm {num1, num2, num3, ...}" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " algorithm \"num1 num2 num3 ...\"" << std::endl;
         return 1;
     }
 
@@ -34,13 +54,12 @@ int main(int argc, char* argv[]) {
     std::vector<int> arr;
     if (argc > 2) {
         std::string input = argv[2];
-        std::istringstream iss(input.substr(1, input.size() - 2));
+        std::replace(input.begin(), input.end(), '{', ' ');
+        std::replace(input.begin(), input.end(), '}', ' ');
+        std::istringstream iss(input);
         int num;
         while (iss >> num) {
             arr.push_back(num);
-            if (iss.peek() == ',') {
-                iss.ignore();
-            }
         }
     } else {
         arr = defaultArray;
@@ -54,8 +73,18 @@ int main(int argc, char* argv[]) {
         mergeSort(arr, 0, arr.size() - 1);
     } else if (algorithm == "quickSort") {
         quickSort(arr, 0, arr.size() - 1);
+    } else if (algorithm == "bubbleSort") {
+        bubbleSort(arr, 0, arr.size() - 1);
+    } else if (algorithm == "selectionSort") {
+        selectionSort(arr, 0, arr.size() - 1);
+    } else if (algorithm == "insertionSort") {
+        insertionSort(arr, 0, arr.size() - 1);
+    } else if (algorithm == "heapSort") {
+        heapSort(arr, 0, arr.size() - 1);
+    } else if (algorithm == "radixSort") {
+        radixSort(arr, 0, arr.size() - 1);
     } else {
-        std::cerr << "Invalid algorithm. Supported algorithms: mergeSort, quickSort" << std::endl;
+        std::cerr << "Invalid algorithm. Supported algorithms: mergeSort, quickSort, bubleSort, selectionSort, insertionSort, heapSort, radixSort" << std::endl;
         return 1;
     }
 
