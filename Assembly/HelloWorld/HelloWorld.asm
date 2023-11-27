1212be862e7af7d454a863fac7b23e13a1d8ce7c
zjack8 <61671353+zjack8@users.noreply.github.com>
2023-11-27 13:34:46 +1300

    Conventional Changes

--- Assembly/HelloWorld/HelloWorld.asm
+++ Assembly/HelloWorld/HelloWorld.asm
@@ -1,5 +1,6 @@
 section .data
-    hello db 'Hello World!', 0
+    msg db 'Hello World!', 0  ;string to be printed
+    len equ $ - msg           ;length of the string
 
 section .text
     global _start
@@ -8,8 +9,8 @@ _start:
     ; write the string to stdout
     mov eax, 4            ; syscall number for sys_write
     mov ebx, 1            ; file descriptor 1 is stdout
-    mov ecx, hello        ; pointer to the string
-    mov edx, 13           ; length of the string
+    mov ecx, msg          ; pointer to the string
+    mov edx, len          ; length of the string
     int 0x80              ; make syscall
 
     ; exit the program
