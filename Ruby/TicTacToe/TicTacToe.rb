e275dac5a9ffd96204ad06b975e0e304cfa31ca2
zjack8 <61671353+zjack8@users.noreply.github.com>
2023-11-27 13:49:45 +1300

    more methods

--- Ruby/TicTacToe/TicTacToe.rb
+++ Ruby/TicTacToe/TicTacToe.rb
@@ -1,28 +1,70 @@
 
 def main
-    # Initialise Game 
+    # Initialise Game
     turn = chooseTeam
 
     puts "==============="
-    while not isWinner:
+    while not isWinner
         turn = turn + 1
         nextTurn(turn)
         printBoard
         puts "==============="
-        
+    end
+
     puts getWinner
+end
 
 def chooseTeam
     # TODO
-
-def isWinner
-    # TODO
+end
 
 def nextTurn(turn)
     # TODO
+end
 
-def printBoard
+def userTurn
     # TODO
+end
+
+def cpuTurn
+    # TODO
+end
+
+def applyIfValid(pos, team)
+    # TODO
+end
 
 def getWinner
     # TODO
+end
+
+def isWinner
+    # TODO
+end
+
+def checkRows
+    # TODO
+end
+
+def checkColumns
+    # TODO
+end
+
+def checkDiagonals
+    # TODO
+end
+
+def isBoardFull
+    # TODO
+end
+
+def printBoard
+    # TODO
+end
+
+def getRandomNumberInRange(min, max)
+    # TODO
+end
+
+# Execute main function
+main
