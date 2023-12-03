ab7dea984bc4126cb7ac2ec7a342bde6688e98c0
zjack8 <mveale8@gmail.com>
2023-12-03 18:21:06 +1300

    ruby ttt

--- Ruby/TicTacToe/TicTacToe.rb
+++ Ruby/TicTacToe/TicTacToe.rb
@@ -15,66 +15,79 @@ def main
 end
 
 def choose_team
-    # TODO
+	# TODO
 end
 
 def next_turn(turn)
-    # TODO
+	if turn % 2 == 0
+		user_turn
+  	else
+		cpu_turn
+	end
 end
 
 def user_turn
-  loop do
-    begin
-      print 'Enter Position: '
-      string_pos = gets.chomp
-      result = Integer(string_pos)
-      return if apply_if_valid(result, user_team)
-    rescue ArgumentError
-      puts 'Wrong Input! Try Again'
-    end
-  end
+  	loop do
+    	begin
+			print 'Enter Position: '
+			string_pos = gets.chomp
+			result = Integer(string_pos)
+			return if apply_if_valid(result, user_team)
+
+			rescue ArgumentError
+			puts 'Wrong Input! Try Again'
+		end
+	end
 end
 
 def cpu_turn
-  puts "CPU's Response:"
-  loop do
-    result = rand(1..9)
-    return if apply_if_valid(result, cpu_team)
-  end
+	puts "CPU's Response:"
+	loop do
+		result = rand(1..9)
+		return if apply_if_valid(result, cpu_team)
+	end
 end
 
 def apply_if_valid(pos, team)
-  # Map number pad positions to Tic Tac Toe board positions
-  position_mapping = [
-    [7, 8, 9],
-    [4, 5, 6],
-    [1, 2, 3]
-  ]
+	# Map number pad positions to Tic Tac Toe board positions
+	position_mapping = [
+		[7, 8, 9],
+		[4, 5, 6],
+		[1, 2, 3]
+	]
 
-  (0..2).each do |row|
-    (0..2).each do |column|
-      if position_mapping[row][column] == pos && board[row][column] == ' '
-        board[row][column] = team
-        return true
-      end
-    end
-  end
-  false
+	(0..2).each do |row|
+		(0..2).each do |column|
+			if position_mapping[row][column] == pos && board[row][column] == ' '
+				board[row][column] = team
+				return true
+			end
+		end
+	end
+	false
 end
 
 def get_winner
-  if winner == user_team
-    'The User Wins!!!'
-  elsif winner == cpu_team
-    'The CPU Wins!!!'
-  else
-    'There is no Winner!'
-  end
+	if winner == user_team
+		'The User Wins!!!'
+	elsif winner == cpu_team
+		'The CPU Wins!!!'
+	else
+		'There is no Winner!'
+	end
 end
   
 
 def is_winner
-    # TODO
+	if winner == user_team:
+        return "The User Wins!!!"
+    
+    elsif winner == cpu_team:
+        return "The CPU Wins!!!"
+    
+    else
+        return "There is no Winner!"
+	end
 end
 
 def check_rows
