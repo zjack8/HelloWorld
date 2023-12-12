98bebd783100cd513e3239074b6a2dd6c1183c88
zjack8 <61671353+zjack8@users.noreply.github.com>
2023-12-13 08:06:13 +1300

    Add Animal Attributes

--- Java/AnimalFarm/animals/Animal.java
+++ Java/AnimalFarm/animals/Animal.java
@@ -1,5 +1,56 @@
 package Java.AnimalFarm.animals;
 
 public class Animal {
-	
+	// Common attributes
+	private String name;
+	private int age;
+	private int weight;
+
+	// Constructor
+	public Animal(String name, int age, int weight) {
+		this.name = name;
+		this.age = age;
+		this.weight = weight;
+	}
+
+	// Getters and setters for the attributes
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public void eat(int food) {
+
+		this.weight += food;
+
+		System.out.println(name + " is eating.");
+	}
+
+	public void sleep() {
+		System.out.println(name + " is sleeping.");
+	}
+
+	public void makeSound() {
+		System.out.println(name + " is making a sound.");
+	}
+
 }
