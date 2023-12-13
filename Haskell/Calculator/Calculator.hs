54096719f04da7574a2a9145a2ee1c6bfc5b5d49
zjack8 <61671353+zjack8@users.noreply.github.com>
2023-12-13 15:00:08 +1300

    Finish Calculator

--- Haskell/Calculator/Calculator.hs
+++ Haskell/Calculator/Calculator.hs
@@ -1,11 +1,29 @@
+import System.IO
+
 main :: IO ()
 main = do
-    putStrLn "Enter the first number:"
+    putStr "Enter the first number: "
+    hFlush stdout  -- Flush the output buffer
     input1 <- getLine
-    let number1 = read input1 :: Int
+    let number1 = read input1 :: Double
 
-    putStrLn "Enter the second number:"
+    putStr "Enter the second number: "
+    hFlush stdout  -- Flush the output buffer
     input2 <- getLine
-    let number2 = read input2 :: Int
+    let number2 = read input2 :: Double
 
     putStrLn ("You entered: " ++ show number1 ++ " and " ++ show number2)
+
+    putStr "Enter an operator (+ - * /): "
+    hFlush stdout  -- Flush the output buffer
+    operator <- getLine
+    
+    let result = case operator of
+            "+" -> number1 + number2
+            "-" -> number1 - number2
+            "*" -> number1 * number2
+            "/" -> if number2 /= 0 then number1 / number2 else error "Cannot divide by zero."
+            _   -> error "Invalid operator. Please enter +, -, *, or /."
+
+    putStrLn ("Result: " ++ show result)
+
