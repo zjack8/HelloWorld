6d85e5f3950e6311cd44cac22edb57586991dac8
zjack8 <mveale8@gmail.com>
2023-12-17 17:00:23 +1300

    Add Calculator

--- Kotlin/Calculator/Calculator.kt
+++ Kotlin/Calculator/Calculator.kt
@@ -1,6 +1,29 @@
-fun main() {
-    print("Enter something: ")
-    val userInput = readLine()
+import java.util.*
 
-    println("You entered: $userInput")
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    print("Enter the first number: ")
+    System.out.flush()
+    val number1 = scanner.nextLine().toDouble()
+
+    print("Enter the second number: ")
+    System.out.flush()
+    val number2 = scanner.nextLine().toDouble()
+
+    println("You entered: $number1 and $number2")
+
+    print("Enter an operator (+ - * /): ")
+    System.out.flush()
+    val operator = scanner.nextLine()
+
+    val result = when (operator) {
+        "+" -> number1 + number2
+        "-" -> number1 - number2
+        "*" -> number1 * number2
+        "/" -> if (number2 != 0.0) number1 / number2 else error("Cannot divide by zero.")
+        else -> error("Invalid operator. Please enter +, -, *, or /.")
+    }
+
+    println("Result: $result")
 }
